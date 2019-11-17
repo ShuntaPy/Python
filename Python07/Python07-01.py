562d883a2cceb702f3b71832fa87f94da8671773
shunshunISC <bjso1290420@gn.iwasaki.ac.jp>
2019-11-17 22:06:28 +0900

    7章

---
+++ Python07/Python07-01.py
@@ -0,0 +1,6 @@
+def add_10(num):
+    add_num = int(num)+10
+    return add_num
+
+
+print(add_10("10"))
---
+++ Python07/Python07-03.py
@@ -0,0 +1,11 @@
+def add_10(num):
+    if not isinstance(num, int):
+        print("Invalid num")
+        return False
+    add_num = num+10
+    print("add_num is {}".format(add_num))
+    return add_num
+
+
+add_10(10)
+add_10("二十")
