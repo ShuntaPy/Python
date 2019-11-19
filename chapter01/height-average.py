018097b729a7be874f9941e9a429d26b9e9a2373
shunshunISC <bjso1290420@gn.iwasaki.ac.jp>
2019-11-19 15:52:35 +0900

    修正

--- chapter01/height-average.py
+++
@@ -1,9 +0,0 @@
-heigths={"taro": 168,"jiro": 171,"takashi": 165}
-
-total=0
-for i in heigths.values():
-    total += i
-
-average=total/len(heigths)
-
-print("平均身長は{0}cmです。".format(average))
--- chapter07/Python07-04.py
+++
@@ -1,6 +0,0 @@
-def add_10(num):
-    add_num = int(num)+10
-    return add_num
-
-
-print(add_10("10"))
