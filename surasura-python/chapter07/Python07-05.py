b361c94654591660e68e7c6b88009746ec00bb9f
shunshunISC <bjso1290420@gn.iwasaki.ac.jp>
2019-11-19 15:53:23 +0900

    修正

---
+++ surasura-python/chapter07/Python07-05.py
@@ -0,0 +1,11 @@
+def add_10(num):
+    add_num = int(num)+10
+    return add_num
+
+
+print(add_10("10"))
+
+fruits=["apple","orange"]
+for fruit in fruits:
+
+    print(fruit)
---
+++ surasura-python/chapter07/Python07-06.py
@@ -0,0 +1,11 @@
+def add_10(num):
+    try:
+        add_num = num+10
+        print("aad_num is {}".format(add_num))
+        return add_num
+
+    except:
+        print("Error!")
+
+
+add_10(10)
